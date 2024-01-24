e238cddee3bc219866f8b52bb4b4553fe556a9bc
Esra Garcia Gorman <esragarcia@hotmail.com>
2024-01-24 21:14:13 +0100

    ViewBinding, ListView ornegi

---
+++ app/src/main/java/com/esragg/landmarkbook/MainActivity.java
@@ -0,0 +1,61 @@
+ package com.esragg.landmarkbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.esragg.landmarkbook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+ public class MainActivity extends AppCompatActivity {
+
+     private ActivityMainBinding binding;
+    ArrayList<Landmark> landmarkArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        landmarkArrayList = new ArrayList<>();
+
+        //Data
+        Landmark pisa = new Landmark("Pisa","Italy", R.drawable.pisa);
+        Landmark eiffel = new Landmark("Eiffel", "France", R.drawable.eiffel);
+        Landmark colosseum = new Landmark("Colosseum","Italy", R.drawable.colosseum);
+        Landmark londonBridge = new Landmark("London Bridge", "UK", R.drawable.londonbridge);
+
+        landmarkArrayList.add(pisa);
+        landmarkArrayList.add(eiffel);
+        landmarkArrayList.add(colosseum);
+        landmarkArrayList.add(londonBridge);
+
+        //Adapter
+        //ListView
+
+        //mapping
+        ArrayAdapter  arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                landmarkArrayList.stream().map(landmark -> landmark.name).collect(Collectors.toList())
+        );
+        binding.listView.setAdapter(arrayAdapter);
+
+        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                //Toast.makeText(MainActivity.this, landmarkArrayList.get(position).name, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                intent.putExtra("landmark",landmarkArrayList.get(position));
+                startActivity(intent);
+            }
+        });
+    }
+}
